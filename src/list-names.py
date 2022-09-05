185e17f9947b9e61690d2b1aaa5803ceeb3921fa
Goncalo Marcelino <gmmarcelino8@gmail.com>
2022-09-05 12:37:49 +0200

    completed exercises

--- src/list-names.py
+++ src/list-names.py
@@ -10,7 +10,9 @@ def main():
     )
     args = argparser.parse_args()
 
-    print(f"Now I need to process the records in {args.fastq}")
+    for line in args.fastq:
+        if line.startswith("@"):
+            print(line[1:].strip())
 
 
 if __name__ == '__main__':
